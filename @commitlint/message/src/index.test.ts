2e9d32f67420677fe95bc797faa32a803fa9d076
Cedric van Putten <me@bycedric.com>
2019-07-27 11:28:41 +0000

    refactor: port message to typescript

---
+++ @commitlint/message/src/index.test.ts
@@ -0,0 +1,13 @@
+import message from '.';
+
+test('should return an empty string for empty input', () => {
+	expect(message()).toBe('');
+});
+
+test('should return an empty string for empty input array', () => {
+	expect(message([])).toBe('');
+});
+
+test('should filter falsy values', () => {
+	expect(message([null, 'some', null, 'message', null])).toBe('some message');
+});
